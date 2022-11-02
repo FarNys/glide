42fc01fec15083ced71f7a2debc952536de079af
Farid Salmani <faridsalmaniy@gmail.com>
2022-11-02 17:06:45 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import DataEditor, { GridCellKind } from "@glideapps/glide-data-grid";
+import React, { useCallback } from "react";
+import "@glideapps/glide-data-grid/dist/index.css";
+import { data } from "./Data/data";
+const App = () => {
+  const getContent = React.useCallback((cell) => {
+    const [col, row] = cell;
+    const dataRow = data[row];
+    const indexes = ["name", "company", "email", "phone"];
+    const d = dataRow[indexes[col]];
+    return {
+      kind: GridCellKind.Text,
+      allowOverlay: true,
+      readonly: false,
+      displayData: d,
+      data: d,
+    };
+  }, []);
 
-function App() {
+  const onCellEdited = React.useCallback((cell, newValue) => {
+    if (newValue.kind !== GridCellKind.Text) {
+      // we only have text cells, might as well just die here.
+      return;
+    }
+
+    const indexes = ["name", "company", "email", "phone"];
+    const [col, row] = cell;
+    const key = indexes[col];
+    data[row][key] = newValue.data;
+  }, []);
+
+  const columnResizeHandler = (props, x, t, a) => {
+    console.log(props, x, t, a);
+  };
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <h1>Test Glide</h1>
+      <DataEditor
+        getCellContent={getContent}
+        columns={columns}
+        rows={data.length}
+        onCellEdited={onCellEdited}
+        onColumnResize={columnResizeHandler}
+      />
     </div>
   );
-}
+};
 
 export default App;
+
+const columns = [
+  {
+    title: "Name",
+    id: "name",
+    width: 150,
+  },
+  {
+    title: "Company",
+    id: "company",
+    width: 150,
+  },
+  {
+    title: "Email",
+    id: "email",
+    width: 150,
+  },
+  {
+    title: "Phone",
+    id: "phone",
+    width: 150,
+  },
+];
